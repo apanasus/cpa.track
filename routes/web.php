e9c899c57593a5b6145280e2b6fbb5d032783131
apanaSUS <apanasHD2@gmail.com>
2024-05-25 01:28:42 +0300

    1

--- routes/web.php
+++ routes/web.php
@@ -34,5 +34,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
+Route::resource('partners', PartnerController::class);
+Route::resource('offers', OfferController::class);
+Route::resource('clicks', ClickController::class);
+Route::resource('conversions', ConversionController::class);
 require __DIR__.'/auth.php';
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
@@ -8,13 +8,6 @@ export default defineConfig({
             input: 'resources/js/app.js',
             refresh: true,
         }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
+        vue(),
     ],
 });
